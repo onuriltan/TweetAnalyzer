211b074640a996386730f8304768cf818fe93996
OnurIltan <onur.iltan@ozu.edu.tr>
2016-07-23 10:08:17 +0300

    Takig trend topics from Twitter's Turkey woeid

--- src/main/java/model/TrendModel.java
+++ src/main/java/model/TrendModel.java
@@ -11,36 +11,38 @@ import javax.swing.JScrollPane;
 import ozu.tweetanalyzer.TrendTopic;
 
 public class TrendModel extends JPanel {
-	
-	  public JList list;
-	  TrendTopic trend= new TrendTopic();
-		
-	  DefaultListModel model;
 
-	  int counter = 15;
 
-	  public TrendModel() {
+	private static final long serialVersionUID = 1L;
+	public JList list;
+	TrendTopic trend= new TrendTopic();
+
+	DefaultListModel model;
+
+	int counter = 15;
+
+	public TrendModel() {
 		trend.getTrendsFromTwitter();
-	    setLayout(new BorderLayout());
-	    model = new DefaultListModel();
-	    list = new JList(model);
-	    JScrollPane pane = new JScrollPane(list);
-	    
-	    for (String t : trend.getTrendList()) {
-	    	model.addElement(t);              
-        }    
-	      
+		setLayout(new BorderLayout());
+		model = new DefaultListModel();
+		list = new JList(model);
+		JScrollPane pane = new JScrollPane(list);
 
-	/*    addButton.addActionListener(new ActionListener() {
+		for (String t : trend.getTrendList()) {
+			model.addElement(t);              
+		}    
+
+
+		/*    addButton.addActionListener(new ActionListener() {
 	      public void actionPerformed(ActionEvent e) {
 	        model.addElement("Element " + counter);
 	        counter++;
 	      }
 	    });
-	 */ 
+		 */ 
 
-	    add(pane, BorderLayout.NORTH);
-	   
-	    
-	  }
+		add(pane, BorderLayout.NORTH);
+
+
+	}
 }
--- src/main/java/ozu/tweetanalyzer/TrendTopic.java
+++ src/main/java/ozu/tweetanalyzer/TrendTopic.java
@@ -2,6 +2,8 @@ package ozu.tweetanalyzer;
 
 import java.util.ArrayList;
 
+import twitter4j.Location;
+import twitter4j.ResponseList;
 import twitter4j.Trend;
 import twitter4j.Trends;
 import twitter4j.Twitter;
@@ -10,36 +12,63 @@ import twitter4j.TwitterFactory;
 
 public class TrendTopic {
 	ArrayList<String> trendTopicList = new ArrayList<String>();
-	
+
 	public void getTrendsFromTwitter(){
+
 		TwitterAuthorization authorize = new TwitterAuthorization();
-		 try {
-			             Twitter twitter = new TwitterFactory(authorize.getCb().build()).getInstance();
-			             Trends trends = twitter.getPlaceTrends(1);
-			          //   System.out.println("Showing current trends");
-			          //  System.out.println("As of : " + trends.getAsOf());
-			             for (Trend trend : trends.getTrends()) {
-			               //  System.out.println(" " + trend.getName());
-			                 trendTopicList.add(trend.getName());
-			             }
-			          //   System.out.println("done.");
-			             
-			         } catch (TwitterException e) {
-			             e.printStackTrace();
-			             System.out.println("Failed to get current trends: " + e.getMessage());
-			            
-			         }
+
+
+		try {
+			Twitter twitter = new TwitterFactory(authorize.getCb().build()).getInstance();
+			int turkeyTrends = getTrendsFromTurkey(twitter);
+			Trends trends = twitter.getPlaceTrends(turkeyTrends);
+
+			for (Trend trend : trends.getTrends()) {
+				trendTopicList.add(trend.getName());
+			}
+
+		} catch (TwitterException e) {
+			e.printStackTrace();
+			System.out.println("Failed to get current trends: " + e.getMessage());
+
+		}
 	}
-	
+
+
+
+
+	public int getTrendsFromTurkey(Twitter twitter){
+		int woeid = 1;
+		try {
+			ResponseList<Location> locations;
+			locations = twitter.getAvailableTrends();
+			System.out.println("Showing the locations that Twitter has trending topic information for Turkey.");
+			for (Location location : locations) {
+				if(location.getName().toUpperCase().equals("TURKEY")){
+					System.out.println(location.getName() + " (woeid:" + location.getWoeid() + ")");
+					woeid = location.getWoeid();
+
+				}
+			}
+			System.out.println("done.");
+		} catch (TwitterException te) {
+			te.printStackTrace();
+			System.out.println("Failed to get trends: " + te.getMessage());
+		}
+		return woeid;
+	}
+
+
+
+
 	public ArrayList<String> getTrendList(){
 		return trendTopicList;
 	}
-	
+
 	public void printTrendList(){
 		for (String trend : trendTopicList) {
-              System.out.println(trend);              
-          }
+			System.out.println(trend);              
+		}
 	}
-	
-	
+
 }
